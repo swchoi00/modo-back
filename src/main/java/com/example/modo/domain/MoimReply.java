ff31d73b8fb97f58839c1a3a558282f14dd03ec0
swchoi00 <celpic-@naver.com>
2024-06-12 03:58:07 +0900

    0612 회의 전 푸쉬

--- src/main/java/com/example/modo/domain/MoimReply.java
+++ src/main/java/com/example/modo/domain/MoimReply.java
@@ -1,54 +1,48 @@
 package com.example.modo.domain;
 
 import java.sql.Timestamp;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-
+import javax.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonFormat;
-
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "moimReply")
 @SequenceGenerator(
-		name = "MOIMREPLY_SEQ_GENERATOR",
-		sequenceName = "MOIMREPLY_SEQ",
-		initialValue = 1, allocationSize = 1)
+    name = "MOIMREPLY_SEQ_GENERATOR",
+    sequenceName = "MOIMREPLY_SEQ",
+    initialValue = 1, allocationSize = 1)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "rno")
 public class MoimReply {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MOIMREPLY_SEQ_GENERATOR")
-	private Long rno;
-	
-	@Column(nullable = false)
-	private String content;
-	
-	@CreationTimestamp
-	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-	private Timestamp createDate;
-	
-	@JsonBackReference
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "moim_id")
-	private Moim moim;
-	
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "moim_member_id")
-	private MoimMember moimMember;
-	
-	
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MOIMREPLY_SEQ_GENERATOR")
+    private Long rno;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private Timestamp createDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "moim_id")
+    private Moim moim;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "moim_member_id")
+    private MoimMember moimMember;
+
+    @Override
+    public String toString() {
+        return "MoimReply{" +
+               "rno=" + rno +
+               ", content='" + content + '\'' +
+               ", createDate=" + createDate +
+               '}';
+    }
 }
