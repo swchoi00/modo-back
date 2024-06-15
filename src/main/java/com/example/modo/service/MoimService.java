f87d94cafe5de7af7ef218de2fe292f308b8cdd0
swchoi00 <celpic-@naver.com>
2024-06-15 17:55:24 +0900

    moimMember 수정

--- src/main/java/com/example/modo/service/MoimService.java
+++ src/main/java/com/example/modo/service/MoimService.java
@@ -223,7 +223,9 @@ public class MoimService {
 //    	System.out.println(moimComm);
     	Long memberId = moimComm.getAuthorid();
     	Member member =  memberRepository.findById(memberId).get();
-    	moimComm.setMember(member);
+    	MoimMember moimMember = moimMemberRepository.findById(memberId).get();
+    	
+    	moimComm.setMoimMember(moimMember);
     	System.out.println(moimComm);
     	moimCommRepository.save(moimComm);
     	
